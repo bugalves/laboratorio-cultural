f60f80fd94cb1375a6c828743934c2b067d222a7
Diogo Gonçalves <107892220+bugalves@users.noreply.github.com>
2026-05-14 15:33:31 +0100

    refactor: fix imports from static files and remove unused app 'users'

--- apps/users/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def login(request):
-    events = []
-    return render(request, 'users/login.html')
--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.core',
-    'apps.users'
 ]
 
 MIDDLEWARE = [
